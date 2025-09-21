771a135e2b7010d4857f6e11ed1de2583dca8a69
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2025-09-21 16:37:29 +0800

    basic login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
+
+function Title() {
+	return (
+		<div className="Title">
+			<h1>Kernel Quest</h1>
+		</div>
+	);
+}
+
+function UserInfo() {
+	const [formData, setFormData] = useState({
+		firstName: "",
+		lastName: "",
+	});
+
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("Submitted:", formData);
+	};
+
+	return (
+		<div className="form-container">
+			<h3 className="form-title">Enter Your Name</h3>
+			<form onSubmit={handleSubmit} className="form">
+				<input
+					type="text"
+					name="firstName"
+					placeholder="First Name"
+					value={formData.firstName}
+					onChange={handleChange}
+					required
+				/>
+
+				<input
+					type="text"
+					name="lastName"
+					placeholder="Last Name"
+					value={formData.lastName}
+					onChange={handleChange}
+					required
+				/>
+
+				<h4>are you aready?</h4>
+
+				<button type="submit">Enter game</button>
+			</form>
+		</div>
+	);
+}
+
 export default function App() {
 	return (
 		<>
-			<h1>lol</h1>
+			<Title></Title>
+			<UserInfo></UserInfo>
 		</>
 	);
 }
